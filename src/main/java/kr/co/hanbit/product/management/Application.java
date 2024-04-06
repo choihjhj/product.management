69cb57e707141083928955b898facc56a1a35986
choihyeji <hyejichoi14@gmail.com>
2024-04-06 18:31:33 +0900

    modelMapper 의존성 주입

--- src/main/java/kr/co/hanbit/product/management/Application.java
+++ src/main/java/kr/co/hanbit/product/management/Application.java
@@ -1,7 +1,10 @@
 package kr.co.hanbit.product.management;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	//new ModelMapper로 매번 DI해도 되지만, 성능상 미리 빈으로 등록해서 사용하는 것이 좋다.
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+				.setFieldMatchingEnabled(true);
+		return modelMapper;
+	}
+
 
 }
